8107b1d13df2157158ced07ba4c1a0b8bbfefe46
k4-100 <k4-100@protonmail.com>
2021-06-11 17:48:04 +0200

    Functions

--- site.php
+++ site.php
@@ -9,31 +9,17 @@
 <body>
  
 
-<form action="site.php" method="post">
+<!-- <form action="site.php" method="post">
     <input type="text" name="student">
     <input type="submit" >
-</form>
+</form> -->
 
 
 <?php 
-    $grades = array(
-        "Jim"=> "A+", 
-        "Pam"=>"B-",
-        "Oscar"=>"C+" 
-    );
-
-
-    // echo $grades[
-    //     "Oscar"
-    // ];
-
-    // echo count($grades);
-
-    echo $grades[ 
-        $_POST["student"] 
-    ];
-
-
+    function sayHi( $name ){
+        echo "Hello  $name";
+    }
+    sayHi( "susu" );
 ?>
 
 
